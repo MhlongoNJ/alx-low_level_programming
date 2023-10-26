089a42d5a90640fc4d4cf314e8e378e92ce44b1a
Ndumiso Julius Mhlongo <MhlongoNdumisoJulius@gmail.com>
2023-10-26 04:31:17 +0200

    Add files via upload

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* print_listint - prints all the elements of a listint_t list
+* @h: pointer to the first node
+*
+* Return: size of list
+*/
+
+size_t print_listint(const listint_t *h)
+
+{
+size_t j = 0;
+
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+j++;
+}
+return (j);
+}
