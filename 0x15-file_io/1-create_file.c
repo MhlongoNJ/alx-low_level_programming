77a48dcc5af931fd02fec705d00830fd8eaaa633
MhlongoNJ <MhlongoNdumisoJulius@gmail.com>
2023-11-06 18:12:47 -0800

    updates

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,10 +22,12 @@ return (i);
 
 /**
 * create_file - a function that creates a file.
-* @filename: where filename is the name of the file to create, if the file already exists, truncate it, if filename is NULL return -1
+* @filename: where filename is the name of the file to create,
+* if the file already exists, truncate it, if filename is NULL return -1
 * @text_content: is a NULL terminated string to write to the file
 *
-* Return: 1 on success, -1 on failure (file can not be created, file can not be written, write “fails”, etc…
+* Return: 1 on success, -1 on failure (file can not be created,
+* file can not be written, write “fails”, etc…
 */
 
 int create_file(const char *filename, char *text_content)
